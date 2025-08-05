6aa7fac65c4daf5baa29b72f09a4ad3fc515e731
chain02saw <chain02saw@users.noreply.github.com>
2025-08-06 01:20:27 +0900

    fix: data type

--- src/components/CoinRainContext.tsx
+++ src/components/CoinRainContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, type ReactNode } from 'react';
 
 interface CoinRainContextType {
   isCoinRainActive: boolean;
